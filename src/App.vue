03c3c4ef650ccd6d7c74ea3e65f09fd64426cfd3
abdallah abu zaid <abood.99.10.osa@gmail.com>
2023-05-01 22:18:36 +0300

    create new alwan project

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <div>
+    <RouterLink to="/">Home</RouterLink>
+    <RouterLink to="/about">About</RouterLink>
+  </div>
+  <RouterView />
+</template>
+
+<style scoped></style>
